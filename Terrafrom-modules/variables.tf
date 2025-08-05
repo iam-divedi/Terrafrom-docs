f7fe95095f206472084de61233493a11de70f6e3
iam-divedi <69568648+iam-divedi@users.noreply.github.com>
2025-08-05 21:56:49 +0530

    variables.tf

---
+++ Terrafrom-modules/variables.tf
@@ -0,0 +1,14 @@
+variable "ami_value" {
+    description = "value of ami"
+    default = ""
+  
+}
+variable "instance_type_value" {
+  description = "value of instance type"
+  
+}
+variable "subnet_id_value" {
+  description = "value of subnet id"
+
+  
+}
